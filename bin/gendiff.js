3e24c4466587d2558c2828701c2ac4b2cac42936
GatyzkayaGeka <gatyzkayageka@gmail.com>
2023-01-18 01:48:42 +0500

    step 1 add 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,3 +3,7 @@
 import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
+program
+.version('1.0.0')
+.description('Compares two configuration files and shows a difference.')
+program.parse();
