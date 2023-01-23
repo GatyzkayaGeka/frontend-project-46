d19230aeb2118128304de928b48ab4b875d3d420
GatyzkayaGeka <gatyzkayageka@gmail.com>
2023-01-24 00:01:47 +0500

    step 3 try 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
@@ -9,4 +10,9 @@ program
 .option('-f, --format <type>', 'output format',)
 .arguments('<filepath1>')
 .arguments('<filepath2>')
+.action((filepath1, filepath2) => {
+    const options = program.opts();
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
+  });
 program.parse();
