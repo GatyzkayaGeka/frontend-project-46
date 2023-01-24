ad598cbb54d3b59ee2f101c3095267fa09e42d70
GatyzkayaGeka <gatyzkayageka@gmail.com>
2023-01-24 22:35:14 +0500

    step 3 try 3

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import path from 'path';
 import _ from 'lodash';
 import treeDiff from './chowDiff.js';
 import readFile from './seeFile.js';
-import stringify from './stringify.js';
-
-
+import stylish from './stringify.js';
 
 const genDiff = (filepath1, filepath2) => {
   const content1 = readFile(filepath1);
   const content2 = readFile(filepath2);
 
   const difference = treeDiff(content1, content2);
-  const result = stringify(difference);
+  const result = stylish(difference);
 
   return result;
 };
--- src/seeFile.js
+++ src/seeFile.js
@@ -2,6 +2,6 @@ import path from 'path';
 import fs from 'fs';
 
 const getFilePath = (filename) => path.resolve(process.cwd(), filename);
-const readFile = (filename) => fs.readFileSync(getFilePath(filename), 'utf-8');
+const readFile = (filename) => JSON.parse(fs.readFileSync(getFilePath(filename), 'utf-8'));
 
 export default readFile;
